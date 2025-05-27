3e2ea991ea6a8af2d1973a8d8e7d28ef6c9fa555
Justin Emgarten <justin@emgarten.com>
2025-05-27 01:07:04 +0000

    Update to use uv

--- tasks.py
+++ tasks.py
@@ -7,23 +7,23 @@ def lint(c: Context) -> None:
     """
     Format and lint
     """
-    c.run("ruff check src tests tasks.py --fix")
-    c.run("black src tests tasks.py")
+    c.run("uv run ruff check src tests tasks.py --fix")
+    c.run("uv run black src tests tasks.py")
 
 
 @task
 def type_check(c: Context) -> None:
-    c.run("mypy src tasks.py")
+    c.run("uv run mypy src tasks.py")
 
 
 @task
 def test(c: Context) -> None:
-    c.run("pytest tests --disable-warnings -v")
+    c.run("uv run pytest tests --disable-warnings -v")
 
 
 @task
 def service_up(c: Context) -> None:
-    c.run("uvicorn src.local_llm_proxy.proxy_app:app --reload --host 0.0.0.0 --port 8000")
+    c.run("uv run uvicorn src.local_llm_proxy.proxy_app:app --reload --host 0.0.0.0 --port 8000")
 
 
 @task
@@ -46,7 +46,6 @@ def build(c: Context) -> None:
     lint(c)
     type_check(c)
     test(c)
-    docker_build(c)
 
 
 @task
